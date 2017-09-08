b5ab2025f69d3e10a7a5e83288970f73ab77565f
welentsai <welentsai@gmail.com>
2017-09-08 10:16:24 +0800

    add up node moudle "cheerio" for html parsing

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
-console.log("hello world");
+var request = require('request');
+
+/*
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred 
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+  console.log('body:', body); // Print the HTML for the Google homepage. 
+});
+*/
+
+var pm25 = function() {
+  request({
+    url: "http://rate.bot.com.tw/xrt?Lang=zh-TW",
+    method: "GET"
+  }, function(error, response, body) {
+    if (error || !body) {
+      return;
+    }
+    // 爬完網頁後要做的事情
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+    console.log('body:', body); // Print the HTML for the Google homepage. 
+  });
+};
+
+pm25();
