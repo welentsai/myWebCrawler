8ee020ab842ce7c1cdc2db2e4a0fac4248818dfd
welentsai <welentsai@gmail.com>
2017-09-13 15:47:38 +0800

    add up mLab db store for bot exchange rate

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
 
+const moment = require('moment');
+
 const mgLib = require('./lib/mgLib.js');
 
+const botRates = require('./lib/webcrawler/bot_exchangeRate.js');
+
 mgLib.init();
+
+// 上傳今日匯率前, 先檢查今日是否已有上傳(避免重複)
+mgLib.findLastRate(function(err, row) { //抓出最後更新的匯率日期
+
+	if(row === null) {
+		console.log("empty document !!");	
+	} else { 
+		let lastUpdate = moment(row.date).format('YYYY-MM-DD');
+		let today = moment(new Date()).format('YYYY-MM-DD');
+		if(lastUpdate === today) { // 今日已更新
+			console.log("today is already updated !!!");
+			return; // skip following code
+		} 
+	}
+
+	getBoT_Rates();
+});
+
+
+function getBoT_Rates() {
+	botRates.getRates(function(rates) {
+
+		mgLib.updateRates(rates, {upsert:true}, function(err) {
+			if(err) {
+				console.log("update rates error !!");
+			} else {
+				console.log("update rates successfully !!");
+			}
+		});
+
+	});
+}
+
+
+
