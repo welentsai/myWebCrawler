bf42d3aa15b7678c3362e9d867d1d2a101d81af2
welentsai <welentsai@gmail.com>
2017-09-13 13:48:36 +0800

    add up node module moment.js for date format

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
 
-// mongoose 4.10.8
-var mongoose = require('mongoose');
+const mgLib = require('./lib/mgLib.js');
 
-/* 
- * Mongoose by default sets the auto_reconnect option to true.
- * We recommend setting socket options at both the server and replica set level.
- * We recommend a 30 second connection timeout because it allows for 
- * plenty of time in most operating environments.
- */
-var options = { keepAlive: true,
-								reconnectTries: Number.MAX_VALUE,
-								useMongoClient: true};
-
-var mongodbUri = 'mongodb://welen:welen88@ds064198.mlab.com:64198/welendb';
-
-mongoose.connect(mongodbUri, options);
-
-var conn = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-conn.once('open', function() {
-	// we're connected!
-	console.log("MongoDB Connected !");
-});
+mgLib.init();
