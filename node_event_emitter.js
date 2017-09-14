0446826068f6376fd11406a06925ed66b5ee6040
welentsai <welentsai@gmail.com>
2017-09-14 15:21:01 +0800

    add up a event emitter exmaple code

---
+++ node_event_emitter.js
@@ -0,0 +1,13 @@
+
+const EventEmitter = require('events');
+
+// JavaScript ES6 
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('event', () => {
+  console.log('an event occurred!');
+});
+
+myEmitter.emit('event');
